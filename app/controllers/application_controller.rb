5d4cf6d081384f3fa4b64ca5d2e08066899b2ace
Amirbarzeg <abarzegarmo@gmail.com>
2021-07-15 00:27:40 -0400

    edit8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
     #attr_accessor :adress
     skip_before_action :verify_authenticity_token
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::MethodOverride
 end
